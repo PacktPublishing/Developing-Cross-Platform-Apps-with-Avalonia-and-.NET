f1c7f4ecc8d7a137b19bb38b2c5ab2ba15696cc2
npolyak <nick.polyak@verizon.net>
2024-11-29 14:21:24 -0500

    code reformating

--- Demos/BuiltInControls/TextBlockSample/TextBlockSample/Views/MainView.axaml.cs
+++ Demos/BuiltInControls/TextBlockSample/TextBlockSample/Views/MainView.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Input;
 
 namespace TextBlockSample.Views;
 
@@ -7,5 +8,70 @@ public partial class MainView : UserControl
     public MainView()
     {
         InitializeComponent();
+
+        TestTextBlock.PointerPressed +=
+            TestTextBlock_PointerPressed;
+        //TestTextBlock.PointerReleased +=
+        //    TestTextBlock_PointerReleased;
+        //TestTextBlock.PointerEntered +=
+        //    TestTextBlock_PointerEntered;
+        //TestTextBlock.PointerMoved +=
+        //    TestTextBlock_PointerMoved;
+        //TestTextBlock.PointerExited +=
+        //    TestTextBlock_PointerExited;
+        //TestTextBlock.DoubleTapped +=
+        //    TestTextBlock_DoubleTapped;
+    }
+
+    private void TestTextBlock_PointerPressed
+    (
+        object? sender,
+        PointerPressedEventArgs e)
+    {
+        if (e.ClickCount == 3)
+        {
+
+        }
+    }
+
+
+    private void TestTextBlock_PointerReleased
+    (
+        object? sender,
+        PointerReleasedEventArgs e)
+    {
+
+    }
+
+
+    private void TestTextBlock_PointerEntered
+        (object? sender, 
+        PointerEventArgs e)
+    {
+
+    }
+
+    private void TestTextBlock_PointerMoved
+    (
+        object? sender, 
+        PointerEventArgs e)
+    {
+
+    }
+
+    private void TestTextBlock_PointerExited
+    (
+        object? sender, 
+        PointerEventArgs e)
+    {
+
+    }
+
+    private void TestTextBlock_DoubleTapped
+    (
+        object? sender, 
+        TappedEventArgs e)
+    {
+
     }
 }
