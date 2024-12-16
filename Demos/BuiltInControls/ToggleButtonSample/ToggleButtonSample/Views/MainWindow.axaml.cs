23cc9b43166fc6f2d6f5c5539042b749d6470f3a
npolyak <nick.polyak@verizon.net>
2024-12-16 12:07:21 -0500

    initial commit

---
+++ Demos/BuiltInControls/ToggleButtonSample/ToggleButtonSample/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace ToggleButtonSample.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
