5346e8ed763864cdfcf961d05a3416fd4d4233b1
npolyak <nick.polyak@verizon.net>
2024-12-16 18:48:06 -0500

    added TextBox sample

--- Demos/BuiltInControls/TextBlockSample/TextBlockSample/Views/MainView.axaml.cs
+++ Demos/BuiltInControls/TextBlockSample/TextBlockSample/Views/MainView.axaml.cs
@@ -5,34 +5,37 @@ namespace TextBlockSample.Views;
 
 public partial class MainView : UserControl
 {
-public MainView()
-{
-    InitializeComponent();
-
-    TestTextBlock.PointerPressed +=
-        TestTextBlock_PointerPressed;
-    //TestTextBlock.PointerReleased +=
-    //    TestTextBlock_PointerReleased;
-    //TestTextBlock.PointerEntered +=
-    //    TestTextBlock_PointerEntered;
-    //TestTextBlock.PointerMoved +=
-    //    TestTextBlock_PointerMoved;
-    //TestTextBlock.PointerExited +=
-    //    TestTextBlock_PointerExited;
-    //TestTextBlock.DoubleTapped +=
-    //    TestTextBlock_DoubleTapped;
-}
-
-private void TestTextBlock_PointerPressed
-(
-    object? sender,
-    PointerPressedEventArgs e)
-{
-    if (e.ClickCount == 3)
+    public MainView()
     {
+        InitializeComponent();
+        
+        TestTextBlock.PointerPressed +=
+            TestTextBlock_PointerPressed;
+        //TestTextBlock.PointerReleased +=
+        //    TestTextBlock_PointerReleased;
+        //TestTextBlock.PointerEntered +=
+        //    TestTextBlock_PointerEntered;
+        //TestTextBlock.PointerMoved +=
+        //    TestTextBlock_PointerMoved;
+        //TestTextBlock.PointerExited +=
+        //    TestTextBlock_PointerExited;
+        //TestTextBlock.DoubleTapped +=
+        //    TestTextBlock_DoubleTapped;
 
+        TestTextBlock.Focusable = true;
+        TestTextBlock.KeyDown += TestTextBlock_KeyDown;
     }
-}
+
+    private void TestTextBlock_PointerPressed
+    (
+        object? sender,
+        PointerPressedEventArgs e)
+    {
+        if (e.ClickCount == 3)
+        {
+
+        }
+    }
 
 
     private void TestTextBlock_PointerReleased
@@ -71,6 +74,14 @@ private void TestTextBlock_PointerPressed
     (
         object? sender, 
         TappedEventArgs e)
+    {
+        
+    }
+
+    private void TestTextBlock_KeyDown
+    (
+        object? sender, 
+        KeyEventArgs e)
     {
 
     }
