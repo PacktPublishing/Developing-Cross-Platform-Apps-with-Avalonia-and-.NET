5f882b61b7f90513d253e3266031ca3e27981dec
npolyak <nick.polyak@verizon.net>
2024-12-16 22:04:32 -0500

    added list box.

---
+++ Demos/BuiltInControls/ListBoxSample/ListBoxSample/Views/MainView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+using Avalonia.Input;
+
+namespace TextBlockSample.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
