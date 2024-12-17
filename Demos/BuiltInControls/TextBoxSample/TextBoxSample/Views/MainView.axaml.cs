116cf3fb0d85045c8f624817a946aab45a29ff5a
npolyak <nick.polyak@verizon.net>
2024-12-16 20:15:36 -0500

    added TextBox sample

--- Demos/BuiltInControls/TextBoxSample/TextBoxSample/Views/MainView.axaml.cs
+++ Demos/BuiltInControls/TextBoxSample/TextBoxSample/Views/MainView.axaml.cs
@@ -5,73 +5,8 @@ namespace TextBoxSample.Views;
 
 public partial class MainView : UserControl
 {
-public MainView()
-{
-    InitializeComponent();
-
-    TestTextBlock.PointerPressed +=
-        TestTextBlock_PointerPressed;
-    //TestTextBlock.PointerReleased +=
-    //    TestTextBlock_PointerReleased;
-    //TestTextBlock.PointerEntered +=
-    //    TestTextBlock_PointerEntered;
-    //TestTextBlock.PointerMoved +=
-    //    TestTextBlock_PointerMoved;
-    //TestTextBlock.PointerExited +=
-    //    TestTextBlock_PointerExited;
-    //TestTextBlock.DoubleTapped +=
-    //    TestTextBlock_DoubleTapped;
-}
-
-private void TestTextBlock_PointerPressed
-(
-    object? sender,
-    PointerPressedEventArgs e)
-{
-    if (e.ClickCount == 3)
+    public MainView()
     {
-
-    }
-}
-
-
-    private void TestTextBlock_PointerReleased
-    (
-        object? sender,
-        PointerReleasedEventArgs e)
-    {
-
-    }
-
-
-    private void TestTextBlock_PointerEntered
-        (object? sender, 
-        PointerEventArgs e)
-    {
-
-    }
-
-    private void TestTextBlock_PointerMoved
-    (
-        object? sender, 
-        PointerEventArgs e)
-    {
-
-    }
-
-    private void TestTextBlock_PointerExited
-    (
-        object? sender, 
-        PointerEventArgs e)
-    {
-
-    }
-
-    private void TestTextBlock_DoubleTapped
-    (
-        object? sender, 
-        TappedEventArgs e)
-    {
-
+        InitializeComponent();
     }
 }
