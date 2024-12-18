0039f4c33ed8f080ba5920e0756ae0753c76394d
npolyak <nick.polyak@verizon.net>
2024-12-17 20:55:04 -0500

    Window sample

---
+++ Demos/BuiltInControls/WindowSample/WindowSample/Views/MainView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace ButtonSample.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
