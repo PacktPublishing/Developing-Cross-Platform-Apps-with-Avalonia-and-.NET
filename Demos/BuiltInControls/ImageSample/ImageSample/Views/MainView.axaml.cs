cdeeadb6fdd0609ee3c4c870264c79a9c08af4ce
npolyak <nick.polyak@verizon.net>
2024-12-19 15:00:22 -0500

    Image sample

---
+++ Demos/BuiltInControls/ImageSample/ImageSample/Views/MainView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Interactivity;
+
+namespace ImageSample.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
