cd388dd4b605bb626ae0895a2e7e94441145ab8f
npolyak <nick.polyak@verizon.net>
2024-12-20 12:15:26 -0500

    added TabControl

---
+++ Demos/BuiltInControls/TabControlSample/TabControlSample/Views/MainView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Interactivity;
+
+namespace MenuSample.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Demos/BuiltInControls/TabControlSample/TabControlSample/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace MenuSample.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
