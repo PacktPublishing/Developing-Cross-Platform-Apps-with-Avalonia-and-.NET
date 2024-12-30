2491a416848a739d3e41845b2c385c14d15a3c9b
npolyak <nick.polyak@verizon.net>
2024-12-30 17:22:49 -0500

    canvas sample

---
+++ Demos/BuildingBlocks/Panels/CanvasSample/CanvasSample/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace CanvasSample.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Demos/BuildingBlocks/Panels/CanvasSample/CanvasSample/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace CanvasSample.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
