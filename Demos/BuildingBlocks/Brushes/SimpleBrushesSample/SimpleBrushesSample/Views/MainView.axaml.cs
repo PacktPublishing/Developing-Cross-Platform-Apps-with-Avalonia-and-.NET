c86050df57f2b01b91dbb1414eab16b96e714e6e
npolyak <nick.polyak@verizon.net>
2025-01-01 13:34:29 -0500

    simple brushes

---
+++ Demos/BuildingBlocks/Brushes/SimpleBrushesSample/SimpleBrushesSample/Views/MainView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Media;
+
+namespace SimpleBrushesSample.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Demos/BuildingBlocks/Brushes/SimpleBrushesSample/SimpleBrushesSample/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace SimpleBrushesSample.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
