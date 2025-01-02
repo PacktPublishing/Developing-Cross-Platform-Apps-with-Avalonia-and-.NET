1516930275cac14014271a476df0ebf478b49000
npolyak <nick.polyak@verizon.net>
2025-01-02 14:40:16 -0500

    translate transform sample

---
+++ Demos/BuildingBlocks/Transforms/TranslateTransformSample/TranslateTransformSample/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace TranslateTransformSample.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
