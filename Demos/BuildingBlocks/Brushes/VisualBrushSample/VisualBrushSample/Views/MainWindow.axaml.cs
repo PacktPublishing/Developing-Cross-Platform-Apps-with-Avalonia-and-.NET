2ccc86fc0c146ed84980618a051c080db0997965
npolyak <nick.polyak@verizon.net>
2025-01-01 22:42:38 -0500

    visual brush

---
+++ Demos/BuildingBlocks/Brushes/VisualBrushSample/VisualBrushSample/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace VisualBrushSample.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
