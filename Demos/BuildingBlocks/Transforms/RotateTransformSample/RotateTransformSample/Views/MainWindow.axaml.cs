480412a61ed54580d8b2464e51c9436244311d50
npolyak <nick.polyak@verizon.net>
2025-01-02 19:32:01 -0500

    rotate transform.

---
+++ Demos/BuildingBlocks/Transforms/RotateTransformSample/RotateTransformSample/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace RotateTransformSample.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
