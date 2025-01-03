be32accd2bd98ee4e152855515918bf696b2f2c8
npolyak <nick.polyak@verizon.net>
2025-01-02 21:14:08 -0500

    scale transform

---
+++ Demos/BuildingBlocks/Transforms/ScaleTransformSample/ScaleTransformSample/Views/MainView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+using Avalonia.Media;
+
+namespace RotateTransformSample.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
