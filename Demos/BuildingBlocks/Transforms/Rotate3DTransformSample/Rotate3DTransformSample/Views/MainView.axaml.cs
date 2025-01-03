f00034e934117162232f33f24bd0e9d30e2e6c39
npolyak <nick.polyak@verizon.net>
2025-01-02 23:22:47 -0500

    Rottate3DTransformSample

---
+++ Demos/BuildingBlocks/Transforms/Rotate3DTransformSample/Rotate3DTransformSample/Views/MainView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+using Avalonia.Media;
+
+namespace Rotate3DTransformSample.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Demos/BuildingBlocks/Transforms/Rotate3DTransformSample/Rotate3DTransformSample/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Rotate3DTransformSample.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
