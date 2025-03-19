5256f34721b93c4ae58760c1164acef4161fc06d
npolyak <nick.polyak@verizon.net>
2025-03-19 18:50:22 -0400

    attached prop sample

---
+++ Demos/Concepts/AvaProps/AttachedPropSample/AttachedPropSample/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace AttachedPropSample.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Demos/Concepts/AvaProps/AttachedPropSample/AttachedPropSample/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace AttachedPropSample.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
