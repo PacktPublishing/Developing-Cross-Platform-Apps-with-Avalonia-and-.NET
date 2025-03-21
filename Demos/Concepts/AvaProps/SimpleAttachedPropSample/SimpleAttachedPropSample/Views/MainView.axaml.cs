88f2e8b3bd0bf80022fc0c8b45c1a610198253f4
npolyak <nick.polyak@verizon.net>
2025-03-21 17:53:29 -0400

    added ways to use the attached property.

--- Demos/Concepts/AvaProps/SimpleAttachedPropSample/SimpleAttachedPropSample/Views/MainView.axaml.cs
+++ Demos/Concepts/AvaProps/SimpleAttachedPropSample/SimpleAttachedPropSample/Views/MainView.axaml.cs
@@ -6,15 +6,14 @@ namespace SimpleAttachedPropSample.Views;
 
 public partial class MainView : UserControl
 {
-    private const string GREETING_TEXT_BASE =
-        "Hello World";
-
-    int _numberClicks = 0;
-
-    IDisposable? _callbackDisposable = null;
-
     public MainView()
     {
         InitializeComponent();
+
+        //AttachedProps.SetGreetingText(RootPanel, "RootPanel Greetings!")
+
+        //var greetingText = AttachedProps.GetGreetingText(RootPanel);
+
+        //string? mainViewGreetingText = AttachedProps.GetGreetingText(this);
     }
 }
