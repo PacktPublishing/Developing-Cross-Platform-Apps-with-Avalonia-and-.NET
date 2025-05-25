3eb70e2923eb2add6fc73998591c07d0e9cb97fd
npolyak <nick.polyak@verizon.net>
2025-05-25 12:19:47 -0400

    useful XAML features sample

---
+++ Demos/Concepts/XAML/UsefulMarkupExtensionsSample/UsefulMarkupExtensionsSample/Views/GreetingContainer.cs
@@ -0,0 +1,6 @@
+﻿namespace UsefulMarkupExtensionsSample.Views;
+
+public class GreetingContainer
+{
+    public static string Greeting { get; } = "Hello World!";
+}
---
+++ Demos/Concepts/XAML/UsefulMarkupExtensionsSample/UsefulMarkupExtensionsSample/Views/StrContainer.cs
@@ -0,0 +1,11 @@
+﻿namespace UsefulMarkupExtensionsSample.Views;
+
+public class StrContainer
+{
+    public string? Str { get; set; } = null;
+
+    public override string ToString()
+    {
+        return Str ?? "null";
+    }
+}
