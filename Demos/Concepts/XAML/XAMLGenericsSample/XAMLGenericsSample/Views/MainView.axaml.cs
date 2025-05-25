9c4c8e2f5e511a702ab26b8bcecee36165e2f377
npolyak <nick.polyak@verizon.net>
2025-05-25 15:30:23 -0400

    generics sample

---
+++ Demos/Concepts/XAML/XAMLGenericsSample/XAMLGenericsSample/Views/MainView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+using System;
+
+namespace XAMLGenericsSample.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Demos/Concepts/XAML/XAMLGenericsSample/XAMLGenericsSample/Views/MyTuple.cs
@@ -0,0 +1,15 @@
+﻿namespace XAMLGenericsSample.Views;
+
+public class MyTuple<T1, T2>
+    where T1 : notnull
+    where T2 : notnull
+{
+    public T1 Item1 { get; set; }
+
+    public T2 Item2 { get; set; }
+
+    public override string ToString()
+    {
+        return $"MyTuple({typeof(T1).Name}, {typeof(T2).Name}): ({Item1}, {Item2})";
+    }
+}
